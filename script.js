aef620caf9137f7ac18585d85bafbfa631f499b1
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2023-08-03 18:02:35 +0900

    change hover event to click and drag and add pen color option

--- script.js
+++ script.js
@@ -4,65 +4,104 @@ let blockcount=16;
 
 document.documentElement.style.setProperty('--blockcount', blockcount);
 
-let totalblocks = blockcount*blockcount;
-
-for (let i=0; i<totalblocks; i++) {
-    block.push("block"+(i+1));
-};
-
 createGrid();
-    function createGrid(){
-        block=[];
-        document.querySelector('.sqcontainer').innerHTML = "";
-        let totalblocks = blockcount * blockcount;
-        for (let i = 0; i < totalblocks; i++) {
+
+function createGrid(){
+    block=[];
+    document.querySelector('.sqcontainer').innerHTML = "";
+    let totalblocks = blockcount * blockcount;
+
+    for (let i = 0; i < totalblocks; i++) {
         block.push("block" + (i + 1));
-        }
-    
-        //insert that grid into a container div already inside HTML
-        block.forEach(function (blockElement) {
+    }
+
+    //insert that grid into a container div already inside HTML
+    block.forEach(function (blockElement) {
         let blockdiv = document.createElement('div');
         blockdiv.classList.add('block');
         let sqcontainer = document.querySelector('.sqcontainer');
         sqcontainer.appendChild(blockdiv);
-        });
+    }); 
 
-        //create hover event
-        const hovblocks = document.querySelectorAll('.block');
-        hovblocks.forEach(hovblock => {
-        hovblock.addEventListener("mouseover", () => {
-            hovblock.classList.add('hover');
-        });
-        });
+    //create click and drag event
+    const fillblocks = document.querySelectorAll('.block');
+    let isMouseDown = false;
+    document.addEventListener('mousedown', function(event) {
+        if (event.button === 0) {
+            isMouseDown = true;
+        }
+    });
 
-
-        const resetbtn = document.querySelector('#reset');
-
-        resetbtn.addEventListener('click', () => {
-            hovblocks.forEach(hovblock => {
-            hovblock.classList.remove('hover');
-            });
-        });
-    }
-
-
-
-//update on prompt
-const btn = document.querySelector('#boxvalue');
-
-btn.addEventListener('click', () => {
-    const userInput = prompt("How many rows do you want?", "Please enter a number between 1 and 100");
+    document.addEventListener('mouseup', function(event) {
+        if (event.button === 0) {
+            isMouseDown = false;
+        }
+    });
     
-    const newBlockCount = parseInt(userInput);
-    if (!isNaN(newBlockCount) && newBlockCount > 0 && newBlockCount<=100) {
-        blockcount = newBlockCount;
-        console.log(`Block count updated to: ${blockcount}`);
-        //reset CSS property
-        document.documentElement.style.setProperty('--blockcount', blockcount);
-        createGrid()
-    } else {
-        alert ("Invalid input. Please enter a number between 1 and 100");
-    };
+    // Create fill event with pressed left mouse button
+    fillblocks.forEach(fillblock => {
+        fillblock.addEventListener('mouseover', () => {
+            if (isMouseDown) {
+                fillblock.classList.add('fill');
+            };
+        });
+    });
+}
+
+function removeFill() {
+    const fillblocks = document.querySelectorAll('.block');
+    fillblocks.forEach(fillblock => {
+    fillblock.classList.remove('fill');
+    });
+}
+
+// Change from button to slider
+// Update on slider value
+const slider = document.getElementById("area");
+const sliderValue = document.getElementById("sliderValue");
+sliderValue.textContent = slider.value + "x" + slider.value;
+slider.oninput = function() {
+    sliderValue.textContent = slider.value + "x" + slider.value;
+
+    const newBlockCount = parseInt(slider.value);
+
+    blockcount = newBlockCount;
+    console.log(`Block count updated to: ${blockcount}`);
+    //reset CSS property
+    document.documentElement.style.setProperty('--blockcount', blockcount);
+    createGrid()
+};
+
+
+//create clear function
+const clearbtn = document.querySelector('#clear');
+clearbtn.addEventListener('click', () => {
+    removeFill();
+    createGrid();
 });
+
+
+// Add reset all button
+const resetbtn = document.querySelector('#reset');
+resetbtn.addEventListener('click', () => {
+    removeFill();
+    slider.value = 16;
+    sliderValue.textContent = slider.value + "x" + slider.value;
+    blockcount = 16;
+    document.documentElement.style.setProperty('--blockcount', blockcount);
+    document.documentElement.style.setProperty('--pencolor', 'black');
+    createGrid();
+});
+
+//Pen Color Input from HTML
+const colorInput = document.getElementById('pencolor');
+    document.documentElement.style.setProperty('--pencolor', 'black');
+    colorInput.addEventListener('change', function () {
+    document.documentElement.style.setProperty('--pencolor', colorInput.value);
+    createGrid();
+  });
+
+
+
 
 
